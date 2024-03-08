381c1d5d6c159244b6046500d9deb5abc13f0d76
Ahmedglez <ahmediglez@gmail.com>
2024-03-07 22:11:47 -0500

    Refactor count reducer to use createAction before createReducer

--- src/render/redux/reducers/count.ts
+++ src/render/redux/reducers/count.ts
@@ -1,11 +1,13 @@
-import { createReducer, createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const initialState: number = 0;
 
 export const setCounter = createAction<number>("counter/set");
 
 const counterReducer = createReducer(initialState, (builder) => {
-  builder.addCase(setCounter, (state, action) => action.payload);
+  builder.addCase(setCounter, (state, action) => {
+    return action.payload;
+  });
 });
 
 export default counterReducer;
